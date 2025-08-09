880623e96af7f5129cf7267c687d6702a780586a
EduardoLG <jlopez-2023086@kinal.edu.gt>
2025-08-08 20:25:49 -0600

    T44 - Modulo de reportes de venta para el administrador

---
+++ src/main/java/controller/ServletReportes.java
@@ -0,0 +1,80 @@
+
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import model.Usuario;
+import model.Marca;
+import model.Categoria;
+import model.Producto;
+
+
+@WebServlet(name = "ServletReportes", urlPatterns = {"/ServletReportes"})
+public class ServletReportes extends HttpServlet {
+    
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+
+        String nombreReporte = request.getParameter("nombre");
+
+        try {
+            if ("usuarios".equals(nombreReporte)) {
+                generarReporte(response, "ReporteUsuarios.jasper", Usuario.class, "SELECT u FROM Usuario u", "reporte_usuarios.pdf");
+            } else if ("marcas".equals(nombreReporte)) {
+                generarReporte(response, "ReporteMarcas.jasper", Marca.class, "SELECT m FROM Marca m", "reporte_marcas.pdf");
+            } else if ("categorias".equals(nombreReporte)) {
+                generarReporte(response, "ReporteCategorias.jasper", Categoria.class, "SELECT c FROM Categoria c", "reporte_categorias.pdf");
+            } else if ("productos".equals(nombreReporte)) {
+                generarReporte(response, "ReporteProductos.jasper", Producto.class, "SELECT p FROM Producto p", "reporte_productos.pdf");
+            } else {
+                response.getWriter().write("Reporte no válido.");
+            }
+        } catch (JRException e) {
+            e.printStackTrace();
+            response.getWriter().write("Error al generar el reporte.");
+        }
+    }
+    
+    private <T> void generarReporte(HttpServletResponse response, String jasperFile, Class<T> entityClass, String queryString, String pdfFileName) 
+            throws JRException, IOException {
+        
+        List<T> datos = entityManager.createQuery(queryString, entityClass).getResultList();
+        
+        InputStream jasperStream = this.getClass().getResourceAsStream("/reports/" + jasperFile);
+        JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
+        
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(datos);
+        
+        Map<String, Object> parametros = new HashMap<>();
+        
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros, dataSource);
+        
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "inline; filename=\"" + pdfFileName + "\"");
+        
+        OutputStream out = response.getOutputStream();
+        JasperExportManager.exportReportToPdfStream(jasperPrint, out);
+        out.flush();
+        out.close();
+    }
+}
--- src/main/java/dao/TarjetaDAO.java
+++ src/main/java/dao/TarjetaDAO.java
@@ -5,13 +5,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
-import model.Tarjetas;
+import model.Tarjeta;
 
 public class TarjetaDAO {
 
     private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("proyectoBimestralPU");
 
-    public void guardar(Tarjetas tarjeta) {
+    public void guardar(Tarjeta tarjeta) {
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
@@ -28,29 +28,29 @@ public class TarjetaDAO {
         }
     }
 
-    public Tarjetas buscarPorId(int codigoTarjeta) {
+    public Tarjeta buscarPorId(int codigoTarjeta) {
         EntityManager em = emf.createEntityManager();
         try {
-            return em.find(Tarjetas.class, codigoTarjeta);
+            return em.find(Tarjeta.class, codigoTarjeta);
         } finally {
             em.close();
         }
     }
 
-    public List<Tarjetas> listarTodas() {
+    public List<Tarjeta> listarTodas() {
         EntityManager em = emf.createEntityManager();
         try {
-            TypedQuery<Tarjetas> query = em.createQuery("SELECT t FROM Tarjetas t", Tarjetas.class);
+            TypedQuery<Tarjeta> query = em.createQuery("SELECT t FROM Tarjetas t", Tarjeta.class);
             return query.getResultList();
         } finally {
             em.close();
         }
     }
     
-    public List<Tarjetas> listarPorUsuario(int codigoUsuario) {
+    public List<Tarjeta> listarPorUsuario(int codigoUsuario) {
         EntityManager em = emf.createEntityManager();
         try {
-            TypedQuery<Tarjetas> query = em.createQuery("SELECT t FROM Tarjetas t WHERE t.codigoUsuario = :codigoUsuario", Tarjetas.class);
+            TypedQuery<Tarjeta> query = em.createQuery("SELECT t FROM Tarjetas t WHERE t.codigoUsuario = :codigoUsuario", Tarjeta.class);
             query.setParameter("codigoUsuario", codigoUsuario);
             return query.getResultList();
         } finally {
@@ -58,7 +58,7 @@ public class TarjetaDAO {
         }
     }
 
-    public void actualizar(Tarjetas tarjeta) {
+    public void actualizar(Tarjeta tarjeta) {
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
@@ -79,7 +79,7 @@ public class TarjetaDAO {
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
-            Tarjetas tarjeta = em.find(Tarjetas.class, codigoTarjeta);
+            Tarjeta tarjeta = em.find(Tarjeta.class, codigoTarjeta);
             if (tarjeta != null) {
                 em.remove(tarjeta);
             }
